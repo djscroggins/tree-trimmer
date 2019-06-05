ebcd7cc3db7a874fdba467119b190e18ea45cf58
David Scroggins <david.j.scroggins@gmail.com>
2019-06-04 23:13:45 -0500

    Modularized data_preprocessor

--- app/treeTrimmer/core/data_preprocessor.py
+++ app/treeTrimmer/core/data_preprocessor.py
@@ -1,13 +1,44 @@
 import numpy as np
 import pandas as pd
+from pandas.core.frame import DataFrame
 from pytypes import typechecked
 
 
 class DataPreprocessor:
 
     @typechecked
+    def _get_target(self, df: DataFrame, target_index: int) -> np.ndarray:
+        target = df.iloc[:, [target_index]]
+
+        r, c = target.shape
+        if c:
+            target = target.values.reshape(r, )
+
+        return target
+
+    @typechecked
+    def _get_features(self, df: DataFrame, target_index: int) -> DataFrame:
+        print(type(df))
+        feature_indices = [x for x in range(df.shape[1]) if x != target_index]
+        return df.iloc[:, feature_indices]
+
+    @typechecked
     @staticmethod
-    def file_to_numpy(file_path: str, target_index: int) -> dict:
+    def _get_feature_names(features: DataFrame) -> np.ndarray:
+        return features.columns.values
+
+    @typechecked
+    @staticmethod
+    def _get_feature_values(features: DataFrame) -> np.ndarray:
+        return features.values
+
+    @typechecked
+    @staticmethod
+    def _get_labels(target: np.ndarray) -> np.ndarray:
+        return np.unique(target)
+
+    @typechecked
+    def file_to_numpy(self, file_path: str, target_index: int) -> dict:
         # TODO: Add labels?
         """
         Converts file to numpy arrays
@@ -22,16 +53,10 @@ class DataPreprocessor:
         """
         df = pd.read_csv(file_path, header=0)
 
-        target = df.iloc[:, [target_index]]
-
-        r, c = target.shape
-        if c:
-            target = target.values.reshape(r, )
-
-        feature_indices = [x for x in range(df.shape[1]) if x != target_index]
-        features = df.iloc[:, feature_indices]
-        feature_names = features.columns.values
-        features = features.values
-        labels = np.unique(target)
+        target = self._get_target(df, target_index)
+        features = self._get_features(df, target_index)
+        feature_names = self._get_feature_names(features)
+        features = self._get_feature_values(features)
+        labels = self._get_labels(target)
 
         return dict(target=target, features=features, feature_names=feature_names, labels=labels)
