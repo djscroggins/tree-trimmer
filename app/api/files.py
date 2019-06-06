ac991470344b87df3dc90ed6993d12c58f973539
David Scroggins <david.j.scroggins@gmail.com>
2019-06-06 01:46:48 -0500

    Moved upload folder to instance variable in files.py

--- app/api/files.py
+++ app/api/files.py
@@ -25,6 +25,11 @@ file_upload_response = files.model('file_upload_response', {
 
 @files.route('')
 class FileManager(Resource):
+
+    def __init__(self, *args, **kwargs):
+        self.UPLOAD_FOLDER = current_app.config['UPLOAD_FOLDER']
+        super().__init__(*args, **kwargs)
+
     @staticmethod
     def allowed_file(filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
@@ -32,29 +37,28 @@ class FileManager(Resource):
     @files.response(HTTPStatus.CREATED, 'File successfully loaded', file_upload_response)
     @files.response(HTTPStatus.FORBIDDEN, 'Only .csv files currently accepted', file_upload_response)
     def post(self):
-        UPLOAD_FOLDER = current_app.config['UPLOAD_FOLDER']
 
         app_path = Path(__file__).parents[1]
 
-        with open(os.path.join(app_path, UPLOAD_FOLDER + 'data-dict.pickle'), 'rb') as f:
+        with open(os.path.join(app_path, self.UPLOAD_FOLDER + 'data-dict.pickle'), 'rb') as f:
             data_dict = pickle.load(f)
 
         file = request.files['file']
         target_index = loads((request.form['target_index']))
 
-        if not os.path.isdir(UPLOAD_FOLDER):
-            os.makedirs(UPLOAD_FOLDER)
+        if not os.path.isdir(self.UPLOAD_FOLDER):
+            os.makedirs(self.UPLOAD_FOLDER)
 
         if file and self.allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file_path = os.path.join(UPLOAD_FOLDER, filename)
+            file_path = os.path.join(self.UPLOAD_FOLDER, filename)
             file.save(file_path)
 
             dpp = DataPreprocessor()
             numpy_dict = dpp.file_to_numpy(file_path, target_index)
             data_dict.update(numpy_dict)
 
-            with open(UPLOAD_FOLDER + 'data-dict.pickle', 'wb') as f:
+            with open(self.UPLOAD_FOLDER + 'data-dict.pickle', 'wb') as f:
                 pickle.dump(data_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
 
             return marshal(dict(message='File successfully loaded'), file_upload_response), HTTPStatus.CREATED
