3b6a17eadb721dff2d8db712511ef78bc2a1166a
David Scroggins <david.j.scroggins@gmail.com>
2019-06-17 18:16:54 -0500

    set up basic webpack config with React skeleton

---
+++ ui-tree-trimmer/src/TreeTrimmer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FormContainer from './js/components/container/FormContainer';
+
+export default class TreeTrimmer extends React.Component {
+    render() {
+        return <div><FormContainer/></div>;
+    }
+};
---
+++ ui-tree-trimmer/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import TreeTrimmer from './TreeTrimmer.jsx';
+ReactDOM.render(<TreeTrimmer/>, document.getElementById('app'));
