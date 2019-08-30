380635cc4b3971ec8750fb9a85aacdbacbffd129
David Scroggins <david.j.scroggins@gmail.com>
2019-08-30 18:21:52 -0500

    Basic react skeleton

--- ui-tree-trimmer/app/src/TreeTrimmer.jsx
+++ ui-tree-trimmer/app/src/TreeTrimmer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import FormContainer from './js/components/container/FormContainer';
+import {Grommet} from "grommet";
+
+import TopBar from './js/components/TopBar';
 
 export default class TreeTrimmer extends React.Component {
     render() {
-        return <div><FormContainer/></div>;
+        return <Grommet plain><TopBar/></Grommet>;
     }
 };
