e555c58f16f73af11d1a6c097b0a37a892309253
David Scroggins <david.j.scroggins@gmail.com>
2019-08-30 18:25:49 -0500

    Basic SideBar component

--- ui-tree-trimmer/app/src/TreeTrimmer.jsx
+++ ui-tree-trimmer/app/src/TreeTrimmer.jsx
@@ -3,9 +3,13 @@ import FormContainer from './js/components/container/FormContainer';
 import {Grommet} from "grommet";
 
 import TopBar from './js/components/TopBar';
+import SideSummary from './js/components/SideSummary'
 
 export default class TreeTrimmer extends React.Component {
     render() {
-        return <Grommet plain><TopBar/></Grommet>;
+        return <Grommet plain>
+            <TopBar/>
+            <SideSummary/>
+        </Grommet>;
     }
 };
---
+++ ui-tree-trimmer/app/src/js/components/SideSummary.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class SideSummary extends React.Component {
+    render() {
+        return (
+            <div>SideSummary</div>
+        )
+    }
+};
