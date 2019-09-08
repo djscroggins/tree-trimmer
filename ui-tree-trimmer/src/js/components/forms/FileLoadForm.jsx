9d8e5a2d1b9c2b1aefd73c79f4cdfb38036e1207
David Scroggins <david.j.scroggins@gmail.com>
2019-09-08 15:55:01 -0500

    Initialize tree from React init form

--- ui-tree-trimmer/src/js/components/forms/FileLoadForm.jsx
+++ ui-tree-trimmer/src/js/components/forms/FileLoadForm.jsx
@@ -18,20 +18,14 @@ export default class FileLoadForm extends React.Component {
 
   _uploadFile = (e) => {
     e.preventDefault();
-    console.log("_uploadFile");
-    console.log(this.state.selectedFile);
-    console.log(this.state.targetIndex);
 
     const formData = new FormData();
     formData.append("file", this.state.selectedFile);
     formData.append("target_index", this.state.targetIndex);
-    console.log("formData ", formData);
-    //  POST
-    //  On success, this.props.setFileUploaded(true)
+
     fetch("http://localhost:5000/files", {
       mode: "cors",
       method: "POST",
-      // headers: { "Content-Type": "multipart/form-data" },
       body: formData
     }).then(response => {
       console.log(`${response.status}: ${response.statusText}`);
@@ -43,25 +37,22 @@ export default class FileLoadForm extends React.Component {
   };
 
   _saveFile = (e) => {
-    console.log("_saveFile");
-    console.log(e.target.files);
     this.setState({ selectedFile: e.target.files[0] });
   };
 
   _setIndex = (e) => {
-    console.log("_setIndex");
-    console.log(e.target.value);
     this.setState({ targetIndex: e.target.value });
   };
 
   render() {
     const { selectedFile } = this.state;
+    const { fileUploaded } = this.props;
     return (
       <form className='file-load-form' onSubmit={this._uploadFile}>
         <input type='file' name='filePicker' accept='text/csv/*' onChange={this._saveFile} required={true}/>
         <div>Target Index</div>
         <NumberInput defaultValue={0} min={0} name='targetIndex' onChange={this._setIndex}/>
-        {!this.props.fileUploaded ? <button type='submit' disabled={!selectedFile}>Upload File</button> : null}
+        {!fileUploaded ? <button type='submit' disabled={!selectedFile}>Upload File</button> : null}
       </form>);
   }
 };
--- ui-tree-trimmer/src/js/components/forms/TreeInitForm.jsx
+++ ui-tree-trimmer/src/js/components/forms/TreeInitForm.jsx
@@ -3,21 +3,74 @@ import React from "react";
 const NumberInput = require("grommet/components/NumberInput");
 const Select = require("grommet/components/Select");
 const CheckBox = require("grommet/components/CheckBox");
+const Button = require("grommet/components/Button");
 
 export default class TreeInitForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      criterion: { label: "Gini", value: "gini" },
+      maxDepth: 20,
+      minSamplesSplit: 2,
+      minSamplesLeaf: 1,
+      randomState: true
+    };
+  }
+
+  _initializeTree = (e) => {
+    e.preventDefault();
+    const { criterion, maxDepth, minSamplesSplit, minSamplesLeaf, randomState } = this.state;
+    const payload = {
+      criterion: criterion.value,
+      max_depth: maxDepth,
+      min_samples_split: minSamplesSplit,
+      min_samples_leaf: minSamplesLeaf,
+      random_state: randomState
+    };
+
+    fetch("http://localhost:5000/decision-trees", {
+      mode: "cors",
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ "parameters": payload })
+    })
+      .then(response => {
+        console.log(`${response.status}: ${response.statusText}`);
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+      });
+  };
+
+  _setCriterion = (e) => this.setState({ criterion: e.option });
+
+  _setMaxDepth = (e) => this.setState({ maxDepth: e.target.value });
+
+  _setMinSamplesSplit = (e) => this.setState({ minSamplesSplit: e.target.value });
+
+  _setMinSamplesLeaf = (e) => this.setState({ minSamplesLeaf: e.target.value });
+
+  _toggleRandomState = (e) => this.setState({ randomState: e.target.checked });
+
+
   render() {
     return (
-      <form className='file-load-form'>
+      <form className='file-load-form' onSubmit={this._initializeTree}>
         <div>Selection Criterion</div>
-        <Select options={["Gini", "Entropy"]} value='Gini' name='selectionCriterion'/>
+        <Select options={[{ "label": "Gini", "value": "gini" }, { "label": "Entropy", "value": "entropy" }]}
+                value={this.state.criterion.label}
+                placeHolder={"Gini"} name='selectionCriterion' onChange={this._setCriterion}/>
         <div>Max Depth</div>
-        <NumberInput defaultValue={20} min={1} name='maxDepth'/>
+        <NumberInput defaultValue={20} min={1} name='maxDepth' onChange={this._setMaxDepth}/>
         <div>Minimum Samples to Split On</div>
-        <NumberInput defaultValue={2} min={2} name='minSamplesSplit'/>
+        <NumberInput defaultValue={2} min={2} name='minSamplesSplit' onChange={this._setMinSamplesSplit}/>
         <div>Minimum Samples Required in Leaf Nodes</div>
-        <NumberInput defaultValue={1} min={1} name='minSamplesLeaf'/>
+        <NumberInput defaultValue={1} min={1} name='minSamplesLeaf' onChange={this._setMinSamplesLeaf}/>
         {/*<div>Random State</div>*/}
-        <CheckBox label='Random State' name='randomState' toggle={true}/>
+        <CheckBox defaultChecked={true} label='Random State' name='randomState' toggle={true}
+                  onChange={this._toggleRandomState}/>
+        <Button label='Initialize Tree' type='submit'/>
       </form>
     );
   }
