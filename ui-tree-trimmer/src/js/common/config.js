942dc60676af50161cec1312c63b50fe7a3676cb
David Scroggins <david.j.scroggins@gmail.com>
2019-09-10 19:04:29 -0500

    Update confusion matrix end color

--- ui-tree-trimmer/src/js/common/config.js
+++ ui-tree-trimmer/src/js/common/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  "confusionMatrix": { "width": 250, "height": 250, "startColor": "#ffffff", "endColor": "#042E8D" }
+  "confusionMatrix": { "width": 250, "height": 250, "startColor": "#ffffff", "endColor": "#865cd6" }
 };
 
