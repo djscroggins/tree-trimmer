80e9a56ca58888a776ebd2b8fcc7ef09edc3ae7b
David Scroggins <david.j.scroggins@gmail.com>
2020-01-25 16:00:22 -0600

    Clean up

--- backend-tree-trimmer/src/api/files.py
+++ backend-tree-trimmer/src/api/files.py
@@ -1,16 +1,14 @@
 from json import loads
-import os
 from http import HTTPStatus
 from pathlib import Path
-import pickle
 
 from flask import current_app, request
 from flask_restplus import Namespace, Resource, fields, marshal, abort
-from werkzeug.utils import secure_filename
 
 from ..core.data_preprocessor import DataPreprocessor
 from config import config
 from ..common.storage import StorageManager
+
 # from run import initialized_app as app
 
 # from api import data_dict
@@ -30,12 +28,12 @@ file_upload_response = files.model('file_upload_response', {
 class FileManager(Resource):
 
     def __init__(self, *args, **kwargs):
-        self.UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'files_storage/')
         self._storage_manager: StorageManager = current_app.storage_manager
+        self._data_preprocessor = DataPreprocessor()
         super().__init__(*args, **kwargs)
 
     @staticmethod
-    def allowed_file(filename):
+    def allowed_file(filename) -> bool:
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
 
     @files.response(HTTPStatus.CREATED, 'File successfully loaded', file_upload_response)
@@ -46,49 +44,21 @@ class FileManager(Resource):
         self._storage_manager.initialize_user_data(current_user)
         data_dict = self._storage_manager.get_user_data(current_user)
 
-        print(request.headers)
-        print(request.form)
-        print(request.files)
-
         file = request.files['file']
-        print(file)
-        target_index = loads((request.form['target_index']))
-        print(target_index)
 
-        # if not os.path.isdir(self.UPLOAD_FOLDER):
-        #     os.makedirs(self.UPLOAD_FOLDER)
+        target_index = loads((request.form['target_index']))
 
         if file and self.allowed_file(file.filename):
-            # filename = secure_filename(file.filename)
-            # file_path = os.path.join(self.UPLOAD_FOLDER, filename)
-            # file.save(file_path)
+
             file_path: Path = self._storage_manager.save_user_data_file(current_user, file)
 
-            dpp = DataPreprocessor()
             # TODO: Pass Path instance to this method
-            numpy_dict = dpp.file_to_numpy(str(file_path), target_index)
+            numpy_dict = self._data_preprocessor.file_to_numpy(str(file_path), target_index)
             data_dict.update(numpy_dict)
 
             self._storage_manager.update_user_data(current_user, data_dict)
-            # with open(self.UPLOAD_FOLDER + 'data-dict.pickle', 'wb') as f:
-            #     pickle.dump(data_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
 
             return marshal(dict(message='File successfully loaded'), file_upload_response), HTTPStatus.CREATED
 
         else:
             abort(HTTPStatus.FORBIDDEN, 'Only .csv files currently accepted')
-
-
-@files.route('/test')
-class FileTestManager(Resource):
-    def post(self):
-        print('File Upload Test Post')
-        print(request)
-        print(request.headers)
-        print(request.form)
-        print(request.files)
-        # file = request.files['file']
-        # target_index = loads((request.form['target_index']))
-        # print('file, ', file)
-        # print('target_index, ', target_index)
-        return marshal(dict(message='File successfully loaded'), file_upload_response), HTTPStatus.CREATED
