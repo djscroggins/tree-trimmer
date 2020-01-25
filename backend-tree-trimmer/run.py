717fce2f64d409070331d64d1a231e0b4e93d29f
David Scroggins <david.j.scroggins@gmail.com>
2020-01-25 12:04:14 -0600

    Updgrade to Flask 1.1.1

--- backend-tree-trimmer/run.py
+++ backend-tree-trimmer/run.py
@@ -6,7 +6,7 @@ initialized_app = create_app(os.getenv('FLASK_CONFIG', 'dev'))
 
 if __name__ == '__main__':
     initialized_app.run(
-        host=initialized_app.config.get('HOST', '0.0.0.0'),
-        port=initialized_app.config.get('PORT', 80),
+        host=initialized_app.config.get('FLASK_HOST', '0.0.0.0'),
+        port=initialized_app.config.get('FLASK_PORT', 80),
         debug=initialized_app.config.get('DEBUG', False)
     )
